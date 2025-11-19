52bd9d39cad92193f47b631ab06dcc537fc8b91a
Prajithshetty0804 <prajithshetty0804@gmail.com>
2025-11-19 14:27:51 +0530

    Rename PlantLoss to WeeklyLossRecord and update fields

--- insights/models.py
+++ insights/models.py
@@ -1,9 +1,4 @@
-from django.db import models
-
-class PlantLoss(models.Model):
-    plant = models.CharField(max_length=100)
-    week = models.IntegerField()
-    loss = models.FloatField()
-
-    def __str__(self):
-        return f"{self.plant} - Week {self.week}"
+class WeeklyLossRecord(models.Model):
+    plant_name = models.CharField(max_length=100)
+    week_number = models.IntegerField()
+    loss_value = models.FloatField()
