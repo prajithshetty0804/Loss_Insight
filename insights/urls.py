0e4f9a4d7df188ef902b85310fbe2f646d5e8ae9
Prajithshetty0804 <prajithshetty0804@gmail.com>
2025-11-19 14:39:52 +0530

    Fix formatting in urls.py

--- insights/urls.py
+++ insights/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import WeeklyLossAPIView
 
 urlpatterns = [
-    path('weekly-loss/', WeeklyLossAPIView.as_view(), name='weekly-loss'),
+   path('weekly-loss/', WeeklyLossAPIView.as_view(), name='weekly-loss'),
 ]
